20425df27fb2fa007fc5d53ce6e748dd5f01645d
Naysila Renatania <renatanianaysila@gmail.com>
2026-02-22 11:12:28 +0700

    Upload project IsyaratKita pertama

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta charset="utf-8" />
+  <title>IsyaratKita</title>
+
+  <link rel="stylesheet" href="css/globals.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <!-- PAGE 1 -->
+  <div class="HOME-PAGE" id="home">
+    <div class="hero-bg"></div>
+
+    <div class="rectangle"></div>
+    <div class="group">
+      <div class="ellipse"></div>
+      <div class="text-wrapper">IK</div>
+    </div>
+    <div class="div">IsyaratKita</div>
+
+    <a class="text-wrapper-2" href="#home">HOME</a>
+    <a class="text-wrapper-3" href="#tentang">TENTANG</a>
+    <a class="text-wrapper-4" href="#fitur">FITUR</a>
+    <a class="text-wrapper-5" href="#harga">HARGA</a>
+
+    <div class="rectangle-2"></div>
+    <a class="text-wrapper-6" href="/login">MASUK</a>
+
+    <div class="text-wrapper-7">IsyaratKita</div>
+
+    <p class="DESKIPSI-SINGKAT">
+      IsyaratKita adalah platform edukasi berbasis website yang menyediakan pembelajaran bahasa isyarat secara
+      sistematis, interaktif, dan mudah diakses oleh berbagai kalangan.
+    </p>
+
+    <div class="KENALI">
+      <div class="KENALI-2"></div>
+      <div class="text-wrapper-8">Kenali IsyaratKita</div>
+    </div>
+
+    <div class="DAFTAR">
+      <div class="BG-DAFTAR"></div>
+      <div class="text-wrapper-9">Daftar!</div>
+    </div>
+  </div>
+
+
+  <!-- PAGE 2 -->
+  <div class="ABOUT-US-VISI-MISI" id="tentang">
+    <div class="rectangle"></div>
+    <div class="group">
+      <div class="ellipse"></div>
+      <div class="text-wrapper">IK</div>
+    </div>
+    <div class="div">IsyaratKita</div>
+
+    <a class="text-wrapper-2" href="#home">HOME</a>
+    <a class="text-wrapper-3" href="#tentang">TENTANG</a>
+    <a class="text-wrapper-4" href="#fitur">FITUR</a>
+    <a class="text-wrapper-5" href="#harga">HARGA</a>
+
+    <div class="rectangle-2"></div>
+    <a class="text-wrapper-6" href="/login">MASUK</a>
+
+    <div class="ABT-US">
+      <div class="BG-ABT-US"></div>
+      <div class="text-wrapper-7">Tentang IsyaratKita</div>
+      <p class="DESKIPSI-SINGKAT">
+        IsyaratKita adalah platform edukasi berbasis website yang menyediakan pembelajaran bahasa isyarat secara
+        sistematis dan interaktif. Platform ini mendukung komunikasi inklusif melalui materi bertahap serta kuis untuk
+        membantu evaluasi pemahaman pengguna.
+      </p>
+    </div>
+
+    <div class="VISI">
+      <div class="text-wrapper-8">Visi</div>
+      <p class="menjadi-platform">
+        Menjadi platform pembelajaran bahasa isyarat terdepan<br />
+        yang mendorong terciptanya komunikasi inklusif dan
+        kesetaraan bagi penyandang tunarungu dan masyarakat luas
+      </p>
+    </div>
+
+    <div class="MISI">
+      <div class="text-wrapper-9">Misi</div>
+      <p class="menyediakan-materi">
+        Menyediakan materi bahasa isyarat yang terstruktur dan interaktif<br />
+        Mendukung kesetaraan komukasi bagi penyandang tunarungu dan masyarakat luas<br />
+        Menghadirkan sistem pembelajaran berbasis video dan kuis yang interaktif<br />
+        Menyediakan layanan pembelajaran online yang fleksibel dan terjangkau
+      </p>
+    </div>
+
+    <div class="text-wrapper-10">IsyaratKita Menyediakan :</div>
+  </div>
+
+
+  <!-- PAGE 3 -->
+  <div class="FITUR-HARGA-PAKET" id="fitur">
+    <div class="rectangle"></div>
+    <div class="kelas-video">Kelas&nbsp;&nbsp;Video Interaktif</div>
+
+    <div class="div"></div>
+    <div class="kuis-evaluasi">Kuis &amp; Evaluasi Pemahaman</div>
+
+    <div class="rectangle-2"></div>
+    <div class="komunitas-dukungan">Komunitas &amp; Dukungan Belajar</div>
+
+    <div class="rectangle-3"></div>
+
+    <div class="LOGO">
+      <div class="ellipse"></div>
+      <div class="text-wrapper">IK</div>
+      <div class="text-wrapper-2">IsyaratKita</div>
+    </div>
+
+    <!-- MENU KHUSUS PAGE 3 (ANTI TABRAKAN) -->
+    <a class="menu-home" href="#home">HOME</a>
+    <a class="menu-tentang" href="#tentang">TENTANG</a>
+    <a class="menu-fitur" href="#fitur">FITUR</a>
+    <a class="menu-harga" href="#harga">HARGA</a>
+
+    <div class="MASUK">
+      <div class="rectangle-4"></div>
+      <a class="text-wrapper-7" href="/login">MASUK</a>
+    </div>
+
+    <p class="p">
+      Materi bahasa isyarat dari tingkat dasar hingga lanjutan, disajikan melalui video interaktif yang mudah dipahami.
+    </p>
+
+    <p class="text-wrapper-8">
+      Latihan dan kuis interaktif untuk menguji pemahaman serta memantau progres belajar secara sistematis.
+    </p>
+
+    <p class="text-wrapper-9">
+      Ruang diskusi untuk berbagi pengalaman dan membangun komunikasi inklusif bersama sesama pembelajar dan pengajar.
+    </p>
+
+    <div class="community-icon">🤝</div>
+    <div class="quiz-icon">📝</div>
+    <div class="video-icon">🎥</div>
+  </div>
+
+
+  <!-- PAGE 4 -->
+  <div class="PAKET-PAKET" id="harga">
+    <div class="group">
+      <div class="rectangle"></div>
+      <div class="paket-harga">Paket &amp; Harga</div>
+    </div>
+
+    <div class="div"></div>
+
+    <div class="LOGO">
+      <div class="ellipse"></div>
+      <div class="text-wrapper">IK</div>
+      <div class="text-wrapper-2">IsyaratKita</div>
+    </div>
+
+    <a class="text-wrapper-3" href="#home">HOME</a>
+    <a class="text-wrapper-4" href="#tentang">TENTANG</a>
+    <a class="text-wrapper-5" href="#fitur">FITUR</a>
+    <a class="text-wrapper-6" href="#harga">HARGA</a>
+
+    <div class="MASUK">
+      <div class="rectangle-2"></div>
+      <a class="text-wrapper-7" href="/login">MASUK</a>
+    </div>
+
+    <div class="rectangle-3"></div>
+    <div class="rectangle-4"></div>
+
+    <div class="text-wrapper-8">PAKET GRATIS</div>
+    <div class="text-wrapper-9">PAKET PREMIUM</div>
+
+    <div class="text-wrapper-10">RP0</div>
+    <div class="text-wrapper-11">RP30.000</div>
+
+    <p class="akses-video">
+      Akses video pembelajaran dasar Bahasa Isyarat<br />
+      Materi alfabet (A–Z) dan angka<br />
+      Pengenalan kata &amp; gerakan sehari-hari<br />
+      Akses gratis 2 bulan
+    </p>
+
+    <p class="semua-benefit-paket">
+      Semua benefit Paket Gratis<br />
+      Akses video lanjutan &amp; percakapan lengkap<br />
+      Materi kalimat, ekspresi, dan konteks nyata<br />
+      Latihan interaktif &amp; kuis<br />
+      Akses komunitas / mentor<br />
+      Update materi eksklusif Premium<br />
+      Akses selama 6 bulan
+    </p>
+  </div>
+
+
+  <!-- PAGE 5 -->
+  <div class="lastt" id="last">
+    <div class="rectangle-1"></div>
+    <div class="ellipse-1"></div>
+    <div class="ik">IK</div>
+    <div class="isyarat-kita">IsyaratKita</div>
+
+    <a class="home" href="#home">HOME</a>
+    <a class="tentang" href="#tentang">TENTANG</a>
+    <a class="fitur" href="#fitur">FITUR</a>
+    <a class="harga" href="#harga">HARGA</a>
+
+    <div class="rectangle-2"></div>
+    <a class="masuk2" href="/login">MASUK</a>
+
+    <div class="hero-bg"></div>
+
+    <div class="siap-mulai-belajar-bahasa-isyarat">
+      Siap Mulai Belajar Bahasa Isyarat?
+    </div>
+
+    <div class="bergabunglah-bersama-kami-dan-mulai-pahami-bahasa-isyarat-dengan-cara-yang-mudah-inklusif-dan-ramah-untuk-semua">
+      Bergabunglah bersama kami dan mulai pahami Bahasa Isyarat dengan cara yang
+      mudah, inklusif, dan ramah untuk semua.
+    </div>
+
+    <div class="rectangle-9"></div>
+    <div class="gabung-sekarang">Gabung Sekarang</div>
+  </div>
+
+</body>
+</html>
+
