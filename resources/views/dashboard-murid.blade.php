271743b84ce39ac884ea8537808e261d1d73fb27
Naysila Renatania <renatanianaysila@gmail.com>
2026-02-27 10:47:02 +0700

    update

--- resources/views/dashboard-murid.blade.php
+++ resources/views/dashboard-murid.blade.php
@@ -20,8 +20,11 @@
     <!-- SIDEBAR -->
     <aside class="sidebar">
       <div class="brand">
-  <div class="brand-badge">IK</div>
-  <div class="brand-sub">Dashboard Murid</div>
+  <img class="logo-img" src="{{ asset('img/LOGO.png') }}" alt="IsyaratKita">
+  <div class="brand-text">
+    <div class="brand-name">IsyaratKita</div>
+    <div class="brand-sub">Dashboard Murid</div>
+  </div>
 </div>
 
       <nav class="menu">
