c21543e6690d677e48038a65c88ead889151227a
aurelzlsa <aurelzalsabillaaa@gmail.com>
2026-04-19 18:42:52 +0700

    login dummy murid & guru (sementara)

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,28 @@ Route::post('/login', function (Request $request) {
     $email = $request->email;
     $password = $request->password;
 
-    // login dummy sementara tanpa database
-    if ($email === 'admin@gmail.com' && $password === '123456') {
+    // login dummy GURU
+    if ($email === 'guru@gmail.com' && $password === '12345678') {
+        session([
+            'dummy_login' => true,
+            'dummy_user' => [
+                'name' => 'Guru',
+                'email' => 'guru@gmail.com',
+                'role' => 'guru'
+            ]
+        ]);
+
+        return redirect('/dashboard-guru');
+    }
+
+    // login dummy MURID
+    if ($email === 'murid@gmail.com' && $password === '12345678') {
         session([
             'dummy_login' => true,
             'dummy_user' => [
                 'name' => 'Admin',
                 'email' => 'admin@gmail.com',
+                'role' => 'murid'
             ]
         ]);
 
@@ -44,7 +59,7 @@ Route::get('/register', function () {
     return view('register');
 })->name('register');
 
-// TANPA auth middleware dulu
+// Dashboard MURID dengan proteksi session
 Route::get('/dashboard-murid', function () {
     if (!session('dummy_login')) {
         return redirect('/login');
@@ -53,11 +68,16 @@ Route::get('/dashboard-murid', function () {
     return view('dashboard-murid');
 })->name('dashboard.murid');
 
-Route::put('/profil/update', [ProfilController::class, 'update'])->name('profil.update');
-
+// Dashboard GURU dengan proteksi session
 Route::get('/dashboard-guru', function () {
+    if (!session('dummy_login')) {
+        return redirect('/login');
+    }
+
     return view('dashboard-guru');
 })->name('dashboard.guru');
+
+Route::put('/profil/update', [ProfilController::class, 'update'])->name('profil.update');
 
 Route::get('/paket', function () {
     return view('paket');
