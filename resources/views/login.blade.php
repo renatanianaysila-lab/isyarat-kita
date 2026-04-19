08ba1b1f0a6d0981fff958b7cc9ed00b4a324854
aurelzlsa <aurelzalsabillaaa@gmail.com>
2026-04-19 18:26:53 +0700

    edit login jadi lebbih kecil

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,17 +6,28 @@
   <title>Login - IsyaratKita</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel="stylesheet" href="{{ asset('css/globals.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">  
 </head>
 <body>
-
-  <!-- NAVBAR DI ATAS -->
-  @include('navigation-bar')
-
   <!-- HALAMAN LOGIN -->
   <div class="LOGIN">
     <!-- Background -->
     <img class="BACKGROUND" src="{{ asset('img/BACKGROUND.png') }}" alt="background">
+
+    <!-- Navbar -->
+    <nav class="navbar">
+      <div class="logo-container">
+        <img src="{{ asset('img/logo.png') }}" alt="IsyaratKita Logo" class="logo-img">
+      </div>
+
+      <div class="nav-links">
+        <a class="nav-link" href="/#home">HOME</a>
+        <a class="nav-link" href="/#tentang">TENTANG</a>
+        <a class="nav-link" href="/#fitur">FITUR</a>
+        <a class="nav-link" href="/#harga">HARGA</a>
+        <a class="nav-link btn-masuk" href="/register">DAFTAR</a>
+      </div>
+    </nav>
 
     <!-- Main Content Login -->
     <main class="main-content">
@@ -27,12 +38,17 @@
         
         <form class="login-form" method="POST" action="/login">
           @csrf
+          
+          <!-- Email -->
           <div class="input-group">
             <input class="form-input" type="email" name="email" placeholder="Email" required>
           </div>
+
+          <!-- Password -->
           <div class="input-group">
             <input class="form-input" type="password" name="password" placeholder="Password" required>
           </div>
+
           <button type="submit" class="submit-btn">LOG IN</button>
         </form>
         
