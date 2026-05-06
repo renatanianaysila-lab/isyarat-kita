07c3ec8e05067a46fe304f7dd19d2bd2a6f71504
Naysila Renatania <renatanianaysila@gmail.com>
2026-05-06 19:40:42 +0700

    add

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaksi extends Model
 {
+    protected $table = 'transaksi'; // ← tambahan
+
     protected $fillable = ['murid_id', 'paket_id', 'kode_transaksi', 'total_harga', 'metode_pembayaran', 'status_transaksi', 'tanggal_transaksi'];
 
     public function murid()
@@ -16,5 +18,5 @@ class Transaksi extends Model
     public function paket()
     {
         return $this->belongsTo(Paket::class, 'paket_id');
-    }//
+    }
 }
